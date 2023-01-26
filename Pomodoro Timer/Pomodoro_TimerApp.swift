ec83c26b5740b3154b4d4e61b66a5d3ea95af4b9
tisma <tisma@gmail.com>
2023-01-26 12:57:53 +0100

    Initial Commit

---
+++ Pomodoro Timer/Pomodoro_TimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pomodoro_TimerApp.swift
+//  Pomodoro Timer
+//
+//  Created by Mathis Zerari on 26/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct Pomodoro_TimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
